73ecce7815eb8f723e61268222517038e13610a4
Yubel426 <849066607@qq.com>
2024-05-13 16:27:04 +0800

    phone

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -80,6 +80,7 @@ class OptimizationParams(ParamGroup):
         self.opacity_lr = 0.05
         self.scaling_lr = 0.005
         self.rotation_lr = 0.001
+        self.normal_lr = 0.005
         self.percent_dense = 0.01
         self.lambda_dssim = 0.2
         self.lambda_dist = 0.0
