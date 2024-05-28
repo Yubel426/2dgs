a3c9176d0e6cb0848f7ea0328690f24a20486e01
Yubel426 <849066607@qq.com>
2024-05-28 18:51:21 +0800

    add NeuS datasets

--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -10,6 +10,7 @@
 #
 
 import torch
+import cv2s
 import math
 import numpy as np
 from typing import NamedTuple
@@ -75,3 +76,20 @@ def fov2focal(fov, pixels):
 
 def focal2fov(focal, pixels):
     return 2*math.atan(pixels/(2*focal))
+
+
+def load_K_Rt_from_P(P=None):
+    out = cv2.decomposeProjectionMatrix(P)
+    K = out[0]
+    R = out[1]
+    t = out[2]
+
+    K = K / K[2, 2]
+    intrinsics = np.eye(4)
+    intrinsics[:3, :3] = K
+
+    pose = np.eye(4, dtype=np.float32)
+    pose[:3, :3] = R.transpose()
+    pose[:3, 3] = (t[:3] / t[3])[:, 0]
+
+    return intrinsics, pose
